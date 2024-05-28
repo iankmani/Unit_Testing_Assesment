de3cca4214399735611294e9ee0065e67f4809d3
Ian Kimani <ian.kiarie2002@gmail.com>
2024-05-28 16:42:51 +0300

    add function: humanities & Sciences

---
+++ main.js
@@ -0,0 +1,29 @@
+function gradeSciences(marks){
+    if(marks >= 70 && marks <= 100){
+        return "A";
+        } else if(marks >= 60 && marks < 69){
+            return "B";
+            } else if(marks >= 50 && marks < 59){
+                return "C";
+                } else if(marks >= 40 && marks < 49){
+                    return "Pass";
+                    } else {
+                        return "Sup";
+                        }
+ }   
+ 
+function gradeHumanities(marks){
+    if(marks >= 80 && marks <= 100){
+        return "A";
+        } else if(marks >= 70 && marks < 79){
+            return "B";
+            } else if(marks >= 60 && marks < 69){
+                return "C";
+                } else if(marks >= 50 && marks < 59){
+                    return "Pass";
+                    } else {
+                        return "Sup";
+                        }
+}
+
+                
